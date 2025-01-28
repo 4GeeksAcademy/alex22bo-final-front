669428898f60fa06fd7cf8fc5d8459c30058de6e
Luis Alexander Borjas Zamora <119633920+alex22bo@users.noreply.github.com>
2025-01-28 20:16:09 +0000

    Feat: modelado base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -5,6 +5,7 @@ db = SQLAlchemy()
 
 
 class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -25,27 +26,41 @@ class Users(db.Model):
 
 # Db Instagram
 class Posts(db.Model):
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(), unique=False, nullable=False)
     description = db.Column(db.String(), unique=False, nullable=False)
     body = db.Column(db.String(), unique=False, nullable=True)
     date = db.Column(db.DateTime(), unique=False, nullable=False) # TODO # default
     image_url = db.Column(db.String(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('posts_to', lazy='select'))
 
 class Medias(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     media_type = db.Column(db.Enum('image', 'video', 'podcast', name='media_type'), unique=False, nullable=False)
     url = db.Column(db.String(), unique=True, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), unique=True)
+    post_to = db.relationship('Posts', foreign_keys=[post_id], backref=db.backref('media_to', lazy='select'))
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(), unique=False, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('user_to', lazy='select'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    post_to = db.relationship('Posts', foreign_keys=[post_id], backref=db.backref('post_to', lazy='select'))
 
 class Followers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    follower_to = db.relationship('Users', foreign_keys=[follower_id], backref=db.backref('follower_to', lazy='select'))
+    following_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    following_to = db.relationship('Users', foreign_keys=[following_id], backref=db.backref('following_to', lazy='select'))
 
 # Db Star Wars
 class Characters(db.Model):
+    __tablename__ = 'characters'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True, nullable=True)
     height = db.Column(db.String(), unique=False, nullable=False)
@@ -57,6 +72,7 @@ class Characters(db.Model):
     gener = db.Column(db.String(), unique=False, nullable=False)
 
 class Planets(db.Model):
+    __tablename__ = 'planets'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True, nullable=True)
     diameter = db.Column(db.String(), unique=False, nullable=False)
@@ -69,7 +85,15 @@ class Planets(db.Model):
 
 class CharacterFavorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('character_favorite_to', lazy='select'))
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    character_to = db.relationship('Characters', foreign_keys=[character_id], backref=db.backref('character_to', lazy='select'))
 
 class PlanetsFavorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('planet_favorite_to', lazy='select'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    planet_to = db.relationship('Planets', foreign_keys=[planet_id], backref=db.backref('planet_to', lazy='select'))
 
