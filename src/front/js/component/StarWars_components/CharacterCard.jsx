2a3ceb83fb5a492fb5490b054f5245af25e5f1f1
Luis Alexander Borjas Zamora <119633920+alex22bo@users.noreply.github.com>
2025-01-28 17:30:57 +0000

    feat: actualización final front

--- src/front/js/component/StarWars_components/CharacterCard.jsx
+++ src/front/js/component/StarWars_components/CharacterCard.jsx
@@ -1,20 +1,21 @@
 import React, { useContext } from "react";
 import { Context } from "../../store/appContext.js";
 
-export const CharacterCard = ({data}) => {
-    const {store} = useContext(Context)
+export const CharacterCard = ({ data }) => {
+    const { store } = useContext(Context)
 
     const handleDetail = () => { };
 
     return (
-        <div className="card border-warning rounded my-3 mx-2 text-bg-dark">            
-            <div className="card-body">
-                <h2 className="mb-1 fw-bold">{data.name}</h2>
-                {/*<span className="text-primary cursor-pointer" onClick={() => handleDetail(data)}>
-                    <FontAwesomeIcon icon={faCircleInfo} />
-                </span>*/}
-                <a href="#" className="btn btn-warning mt-2">More Info</a>
+        <div className="col">
+            <div className="card border-dark rounded my-3 mx-2 text-bg-dark">
+                <img src="https://starwars-visualguide.com/assets/img/characters/1.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h2 className="mb-1 fw-bold">{data.name}</h2>
+                </div>
             </div>
+
+
         </div>
     )
 }
--- src/front/js/pages/StarWars_Pages/Characters.jsx
+++ src/front/js/pages/StarWars_Pages/Characters.jsx
@@ -3,16 +3,19 @@ import { CharacterCard } from "../../component/StarWars_components/CharacterCard
 import { Context } from "../../store/appContext.js";
 
 export const Characters = () => {
-    const {store} = useContext(Context)
-          
+    const { store } = useContext(Context)
 
-    return(
+
+    return (
         <div className="container bg-dark mb-3">
             <h1 className="text-light text-center pt-4">Character</h1>
-            <div className="row row-cols-1 row-cols-md-3 row cols-lg-4 row-cols-xl-5">
-            {store.dataCharacters.map((data, index) => (<CharacterCard key={index} data={data} />))}
-              
-            </div>     
+            <div className="card-body">
+                <div className="row">
+                    {store.dataCharacters.map((data, index) => (<CharacterCard key={index} data={data} />))}
+                </div>
+
+            </div>
+
         </div>
     )
 }
