f607974a1badcbee4e1f0c2dd5bcd520ea17859d
Luis Alexander Borjas Zamora <119633920+alex22bo@users.noreply.github.com>
2025-02-10 22:42:11 +0000

    feat: private culminado

--- src/front/js/component/Private.jsx
+++ src/front/js/component/Private.jsx
@@ -1,12 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 
 export const Private = () => {
-
     const { store } = useContext(Context);
+    const navigate = useNavigate();
 
+    useEffect(() =>{
+        if (!store.isLogged){
+            navigate('/login')
+        }
+    }, [store.isLogged, navigate])
 
-    return(
-
-    )
-}
+    return store.isLogged ? (
+        <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUTxUQay2IG3GHmq2uppGon9DkJvU0rfopiQ&s" alt="welcome" />
+        </div>
+    ) : null
+};
